5bbcefb897d72011d70d26d64be9a2c7da4238c8
eduardo <eduardodsxavier@gmail.com>
2024-09-10 16:58:11 -0300

    start to work on the app screens

--- App.js
+++ App.js
@@ -1,22 +1,28 @@
 import './gesture-handler';
 
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+import { HomeScreen } from './screens/HomeScreen'
+import { GraphScreen } from './screens/GraphScreen'
+import { DayScreen } from './screens/DayScreen'
+import { ExerciseScreen } from './screens/ExerciseScreen'
+
+const Tab = createBottomTabNavigator()
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Screen name='Home' component={HomeScreen} /> 
+      <Tab.Screen name='Graph' component={GraphScreen} />
+    </NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Day" component={DayScreen} />
+      <Stack.Screen name="Exercise" component={ExerciseScreen} />
+    </Stack.Navigator>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
